457644f721d3debf8ad1a5e300586f8e4a4f7ce7
Aditya Singh <adityasinghdhs@gmail.com>
2023-07-11 22:31:47 -0400

    next card

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Define cards
+/define cards
 let deck = [];
 const suits= ['Hearts, 'Diamonds, 'Clubs', 'Spades'];
 const ranks= ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
@@ -38,3 +38,9 @@ function shuffle(deck) {
   }
   return deck;
 }
+
+//get the next card from the deck
+function getNextCard() {
+  return deck.shift();
+}
+
