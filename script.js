0b167847172ae927240df8a0ddd82be63a0fa421
Aditya Singh <adityasinghdhs@gmail.com>
2023-07-11 22:11:34 -0400

    shuffling

--- script.js
+++ script.js
@@ -31,3 +31,10 @@ document.getElementByID('dealer-hand').textContent = 'Dealer Hand: ' + dealerHan
 }
 
 //function to shuffle the deck
+function shuffle(deck) {
+  for (let i = deck.length -1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [deck[i], deck[j]] = [deck[j], deck[i]];
+  }
+  return deck;
+}
